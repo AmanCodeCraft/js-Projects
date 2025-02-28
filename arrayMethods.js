1153a4b6751d0d9654911e00683b37f40751d3e0
Amandeep Singh <153711495+AmanCodeCraft@users.noreply.github.com>
2025-02-28 16:05:46 +0530

    Add files via upload

---
+++ arrayMethods.js
@@ -0,0 +1,46 @@
+// Array Methods:
+// 11. Add the number 100 to the end of the array 'numbers' using '.push()'.
+// let numbers=[20,20];
+// numbers.push(100)
+// console.log(numbers);
+
+// 12. Remove the last element from the array 'numbers' using '.pop()'.
+// let numbers=[20,10,40];
+// numbers.pop();
+// console.log(numbers);
+
+// 13. Add the number 5 to the beginning of the array 'numbers' using '.unshift()'.
+// numbers.unshift(10);
+// console.log(numbers);
+
+// 14. Remove the first element from the array 'numbers' using '.shift()'.
+// numbers.shift();
+// console.log(numbers);
+
+// 15. Find the index of the number 7 in the array 'numbers' using '.indexOf()'.
+
+// 16. Join all elements of the array 'fruits' into a single string separated by commas using '.join()'.
+// let fruits=["apple","bnana","grapes","leechy","pine-apple"];
+
+// console.log(fruits.join("\n"));
+
+// 17. Sort the array 'numbers' in ascending order using '.sort()'.
+// let numbers=[20,10,40,2,-3];
+// console.log(numbers.sort((a,b)=>a-b));
+
+// 18. Reverse the order of the array 'colors' using '.reverse()'.
+// let colors=["Red","blue","green"];
+// console.log(colors.reverse());
+
+// 19. Create a new array with each element from the array 'numbers' doubled using '.map()'.
+// let numbers=[20,10,40,2,-3];
+// let doubleNumber=numbers.map(n=>n*2);
+// console.log(doubleNumber);
+// console.log(doubleNumber.sort((a,b)=>a-b));
+
+// 20. Filter out all even numbers from the array 'numbers' using '.filter()'.
+//  let numbers=[20,10,40,2,-3];
+//  let evenOnly=numbers.filter(num=>num%2===0);
+//  console.log(`evenOnly ${evenOnly}`);
+ 
+
